7a2ec58a91d7fdea9eb240ed732f16b2922904f7
Truman TIAN <tian@example.com>
2024-10-19 09:32:34 +0000

    wind client and server

---
+++ wind_client.py
@@ -0,0 +1,106 @@
+from pyttkits import kits
+import requests
+import os
+import logging
+from datetime import datetime
+import json
+
+logger = logging.getLogger(__name__)
+
+class WindClient:
+
+    def __init__(self, cached_dir='/data/public//raw/cne/wind/cache'):
+        self.url = 'http://localhost:5000'
+        self.cached_dir = cached_dir
+
+    def _make_post(self, path, data):
+        response = requests.post(self.url + path, json=data)
+        print(response)
+        return response.json()
+
+    def cache_request(self, path, data, to_cache=False):
+        reqhash = kits.make_hash(sorted([(k, v) for k, v in data.items()], key=lambda x: x[0]))
+        cache_file = f'{self.cached_dir}/{reqhash}.json.gz'
+        logger.debug(f'cache_file: {cache_file}')
+        if os.path.exists(cache_file):
+            rspdata =  kits.read_json(cache_file)
+        else:
+            res = self._make_post(path, data)
+            if to_cache:
+                kits.save_json(res, cache_file)
+            rspdata = res
+        return rspdata
+
+    def wsd(self, symbols, fields, begin_date, end_date, options='', to_cache=False):
+        data = {
+            'cmd': 'wsd',
+            'codes': symbols,
+            'fields': fields,
+            'beginTime': begin_date,
+            'endTime': end_date,
+            'options': options
+        }
+        return self.cache_request('/post_data', data, to_cache)
+
+
+    def wset(self, table_name, options='', to_cache=False):
+        data = {
+            'cmd': 'wset',
+            'tableName': table_name,
+            'options': options
+        }
+        return self.cache_request('/post_data', data, to_cache)
+
+    def get_symbols(self, date='', include_BJ=False):
+        import pandas as pd
+        if not date:
+            date = datetime.now().strftime('%Y%m%d')
+
+        data = self.wset('sectorconstituent', f'date={date};sectorid=a001010100000000', to_cache=True)
+
+        df = pd.DataFrame(json.loads(data['data']))
+
+        names = df['wind_code'].tolist()
+        if not include_BJ:
+            names = [name for name in names if not name.endswith('.BJ')]
+
+        return names
+
+    def get_daily_field(self, field, date, start_date=None):
+        import pandas as pd
+        today = datetime.now().strftime('%Y%m%d')
+        to_cache = True
+        if date >= today:
+            to_cache = False
+        symbols = self.get_symbols(date)
+        if start_date is None:
+            start_date = date
+        data = self.wsd(symbols, field, start_date, date, to_cache=to_cache)
+        df = pd.DataFrame(json.loads(data['data']))
+        if start_date == date:
+            df = df.iloc[:, -1]
+        else:
+            df.index = pd.to_datetime(df.index, unit='ms').strftime('%Y%m%d')
+        return df
+
+
+def test_client():
+    cl = WindClient()
+
+    symbols = cl.get_symbols()
+
+    df = cl.get_daily_field(date='20241017', field='close')
+    print(df)
+    pcls_df = cl.get_daily_field(date='20241017', start_date='20240901', field='pre_close')
+    print(pcls_df)
+
+def main():
+    from pyttkits import kits, file as etfile
+    kits.init_logging('debug')
+    args = {'__subcmd__': []}
+    args.update(kits.make_sub_cmd(test_client))
+    args = kits.make_args(args)
+    kits.run_cmds(args)
+
+if __name__ == '__main__':
+    main()
---
+++ wind_serv.py
@@ -0,0 +1,208 @@
+from datetime import datetime
+import logging
+
+def test():
+    from WindPy import w
+
+    #w.start() # 默认命令超时时间为120秒
+    w.start()
+    # 如需设置超时时间可以加入waitTime参数，例如waitTime=60,即设置命令超时时间为60秒
+
+    result = w.isconnected() # 判断WindPy是否已经登录成功
+    print(result) # True
+
+    def func(*args, **kwargs):
+        print(args, kwargs)
+
+    print('pre_close:')
+    data = w.wsd("603817.SH,301168.SZ", "pre_close")
+    print(data)
+
+    print('close')
+    data = w.wsd("603817.SH,301168.SZ", "close")
+    print(data)
+
+    print('open')
+    data = w.wsd("603817.SH,301168.SZ", "open")
+    print(data)
+
+    w.stop()
+
+class MockWindPy:
+
+    def __init__(self):
+        pass
+
+    def wsd(self, codes, fields, begin_time=None, end_time=None, options=''):
+        if fields == '':
+            segs = 'open,high,low,close,volume,amt'.split(',')
+        else:
+            segs = fields.split(',')
+
+        if begin_time is None and end_time is None:
+            begin_time = ''
+            end_time = ''
+
+        if begin_time == '' and end_time == '':
+            begin_time = datetime.now().strftime('%Y-%m-%d')
+            end_time = datetime.now().strftime('%Y-%m-%d')
+
+        dates_range = pd.date_range(begin_time, end_time)
+        df = pd.DataFrame(columns=segs, index=dates_range)
+        return data
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+class WindServer:
+
+    def __init__(self):
+        #from MockWindPy import w ; self.w = MockWindPy()
+        from WindPy import w ; self.w = w
+        self.w.start()
+
+    def __del__(self):
+        self.w.stop()
+
+    def get_data(self, code, field):
+        data = self.w.wsd(code, field)
+        return data
+
+    def handle_request(self, json_data):
+        logging.info('json_data: %s' % json_data)
+        response = {
+            'ok': True,
+        }
+        try:
+            cmd = json_data.get('cmd')
+        except Exception as e:
+            response['ok'] = False
+            response['error'] = str(e)
+            return response
+
+        if cmd == 'wsd':
+            codes = json_data.get('codes', None)
+            fields = json_data.get('fields', '')
+            options = json_data.get('options', '')
+            begin_time = json_data.get('beginTime', None)
+            end_time = json_data.get('endTime', None)
+
+            logging.info('codes: %s, fields: %s, begin_time: %s, end_time: %s' % (codes, fields, begin_time, end_time))
+
+            if begin_time is None and end_time is None:
+                begin_time = ''
+                end_time = ''
+
+            if codes is None or fields is None:
+                response['ok'] = False
+                response['error'] = 'codes or fields is None'
+                return response
+
+            rsp, df = self.w.wsd(codes, fields, begin_time, end_time, options, usedf=True)
+
+            if rsp != 0:
+                response['ok'] = False
+                response['error_no'] = str(rsp)
+                response['error'] = str(df)
+                return response
+
+            df_data = df.to_json()
+            response['data'] = df_data
+            response['wind_ret'] = str(rsp)
+        elif cmd == 'wset':
+            tableName = json_data.get('tableName', None)
+            options = json_data.get('options', None)
+            if tableName is None:
+                response['ok'] = False
+                response['error'] = 'tableName is None'
+                return response
+
+            if options is None:
+                response['ok'] = False
+                response['error'] = 'options is None'
+                return response
+
+            # self.w.wset("sectorconstituent","sectorid=a001010100000000;field=wind_code")
+            rsp, df = self.w.wset(tableName, options, usedf=True)
+            if rsp != 0:
+                response['ok'] = False
+                response['error_no'] = str(rsp)
+                response['error'] = str(df)
+                return response
+            df_data = df.to_json()
+            response['data'] = df_data
+            response['wind_ret'] = str(rsp)
+
+        else:
+            response['ok'] = False
+            response['error'] = 'Unknown cmd: %s' % cmd
+
+        return response
+
+__context__ = {
+}
+
+
+def get_wind_server():
+    global __context__
+    if 'wind_server' not in __context__:
+        __context__['wind_server'] = WindServer()
+    return __context__['wind_server']
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+file_handler = logging.FileHandler('app.log')
+
+# Set the log level
+file_handler.setLevel(logging.DEBUG)
+
+# Create a logging format
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+# Set flush behavior to ensure logs are written immediately
+class FlushFileHandler(logging.FileHandler):
+    def emit(self, record):
+        super().emit(record)
+        self.flush()
+
+# Use the custom handler to flush logs immediately
+flush_file_handler = FlushFileHandler('app.log')
+flush_file_handler.setLevel(logging.DEBUG)
+flush_file_handler.setFormatter(formatter)
+
+# Add the handler to the app's logger
+# app.logger.addHandler(flush_file_handler)
+# app.logger.setLevel(logging.DEBUG)
+
+
+@app.route("/")
+def hello_world():
+    app.logger.warn('hello_world')
+    logging.warn('hello_world')
+    return "<p>Hello, World!</p>"
+
+@app.route("/post_data", methods=['POST'])
+def post_data():
+    # print('post_data')
+    if request.method == 'POST':
+        data = request.get_json()
+        wind_server = get_wind_server()
+        logging.info('data: %s' % data)
+        response = wind_server.handle_request(data)
+        return response
+    else:
+        return json.dumps({'ok': False, 'error': 'Only support POST method'})
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    app.logger.setLevel(logging.DEBUG)
+    app.run(debug=True)
+
+if __name__ == "__main__":
+    main()
